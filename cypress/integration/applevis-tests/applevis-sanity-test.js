52a5aa4b61d265f55d7b33ef7f20976b26e1a172
Vsevolod Popov <sevapopov13@gmail.com>
2023-03-18 20:34:04 +0300

    Sanity tests. Move visit step to beforeEach section.

--- cypress/integration/applevis-tests/applevis-sanity-test.js
+++ cypress/integration/applevis-tests/applevis-sanity-test.js
@@ -1,18 +1,18 @@
 describe('Applevis sanity tests', () => {
-    it('Applevis main page title', () => {
+    beforeEach(() => {
         cy.visit('https://applevis.com');
+    });
+    it('Applevis main page title', () => {
         cy.get('title').invoke('text').should('equal', 'AppleVis');
     });
 
     it('Open apps page', () => {
-        cy.visit('https://applevis.com');
         cy.get("button[aria-label='Main Menu']").click();
         cy.get('a').contains('Apps').invoke('text').should('equal', 'Apps');
         cy.get('a').contains('Apps').click();
     })
 
     it('Open Podcast page', () => {
-        cy.visit('https://applevis.com');
         cy.get("button[aria-label='Main Menu']").click();
         cy.get("button[title='More community resources']").click();
         cy.get('a').contains('Podcast').invoke('text').should('equal', 'Podcast');
