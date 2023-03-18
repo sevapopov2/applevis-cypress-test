e062bef3dc49ac8f99e26a453b0bda6003833669
Vsevolod Popov <sevapopov13@gmail.com>
2023-03-18 20:04:13 +0300

    Sanity tests. Add open Apps page test.

--- cypress/integration/applevis-tests/applevis-sanity-test.js
+++ cypress/integration/applevis-tests/applevis-sanity-test.js
@@ -1,6 +1,13 @@
 describe('Applevis sanity tests', () => {
-    it('Applevis main page title', () =>{
+    it('Applevis main page title', () => {
         cy.visit('https://applevis.com');
-        cy.get('title').invoke('text').should('equal', 'AppleVis')
+        cy.get('title').invoke('text').should('equal', 'AppleVis');
     });
+
+    it('Open apps page', () =>{
+        cy.visit('https://applevis.com');
+        cy.get("button[aria-label='Main Menu']").click();
+        cy.get('a').contains('Apps').invoke('text').should('equal', 'Apps');
+        cy.get('a').contains('Apps').click();
+    })
 });
